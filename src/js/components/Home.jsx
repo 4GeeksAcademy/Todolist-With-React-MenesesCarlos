b5eff6f91f48f8557423217baf6d5e38184a6e44
Carlos-Meneses1 <carlosocharly1@hotmail.com>
2025-09-12 23:33:55 +0000

    add maquetacion

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,22 +6,15 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container">
+      <h1>My Todos</h1>
+      <ul>
+        <li>Make the bed</li>
+        <li>Walk the dog</li>
+        <li>Pay taxes</li>
+        <li>Go on vacation</li>
+      </ul>
+    </div>
 	);
 };
 
